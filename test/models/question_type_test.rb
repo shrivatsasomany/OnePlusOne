7a6a781dd6ee90f9c34516cf3223723e9baf40d5
Shrivatsa Somany <shrivatsasomany@gmail.com>
2014-12-09 16:26:39 -0500

    Some more additions Question type

---
+++ test/models/question_type_test.rb
@@ -0,0 +1,17 @@
+# == Schema Information
+#
+# Table name: question_types
+#
+#  id         :integer          not null, primary key
+#  type_text  :string(255)
+#  created_at :datetime
+#  updated_at :datetime
+#
+
+require 'test_helper'
+
+class QuestionTypeTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
